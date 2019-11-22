b11dcb8fa5b2f9a2188761ebc817eab05f7962c2
Dawid Tobor <dawidtobor@op.pl>
2019-11-22 18:27:00 +0100

    Initial Commit

---
+++ C++17/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++17
+//
+//  Created by Dawid on 22/11/2019.
+//  Copyright © 2019 Ishiguro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
